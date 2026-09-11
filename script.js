19fbb7f35093ef732d4899ee293a039b44b69826
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-12 01:57:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const sTag = document.querySelectorAll("strong");
+
 
 function highlight() {
     //Write your code here
+	const sTag = document.querySelectorAll("strong");
 	sTag.forEach(iteams,()=>{
-		iteams.style.color = rgb(0,128,0);
+		iteams.style.color = "rgb(0,128,0)";
 	});
 }
 
 
 function return_normal() {
 	sTag.forEach(iteams,()=>{
-		iteams.style.color = rgb(0,0,0);
+		const sTag = document.querySelectorAll("strong");
+		iteams.style.color = "rgb(0,0,0)";
 	});
 }
